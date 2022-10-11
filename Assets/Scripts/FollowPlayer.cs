9d38ac42e7bcc1c488aceeb09ac124838a9405c3
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-10-11 19:01:53 +0400

    update FollowPlayer.cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,25 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class followPlayer : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public GameObject objectToFollow;
+    [SerializeField] private Transform transformToFollow;
+    [SerializeField] private int height;
 
     private Transform _transform;
-    private Transform _objectTransform;
 
-    void Start()
-    {
+    void Start() {
         _transform = GetComponent<Transform>();
-        _objectTransform = objectToFollow.GetComponent<Transform>();
     }
 
-    void Update()
-    {
-        Vector3 objectPosition = _objectTransform.position;
-        objectPosition.y = 10;
-        _transform.position = objectPosition;
+    void Update() {
+        Vector3 targetPosition = transformToFollow.position;
+        targetPosition.y = height;
 
-        _transform.LookAt(_objectTransform);
+        _transform.position = targetPosition;
+
+        _transform.LookAt(transformToFollow);
     }
 }

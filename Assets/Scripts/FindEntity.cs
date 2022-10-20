c629b010c2de487f097069c95f61c1191ad1fe31
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-10-20 22:45:14 +0400

    FindEntity переписан с использованием UnityEvent

--- Assets/Scripts/FindEntity.cs
+++ Assets/Scripts/FindEntity.cs
@@ -1,66 +1,77 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FindEntity : MonoBehaviour
 {
     [SerializeField] private Transform _transform;
     [SerializeField] private Transform _gunTransform;
-    private List<Entity> _entityList = new List<Entity>();
-    private Entity _closestEntity;
+    [SerializeField] private PlayerMovement _playerMovement;
+    private List<GameObject> _entityList = new List<GameObject>();
+    private GameObject _closestEntity;
+
+    public UnityEvent<GameObject> ClosestEntityChanged;
+
+    private void Awake()
+    {
+        ClosestEntityChanged.AddListener(OnClosestEntityChange);
+    }
+
+    private void OnDestroy()
+    {
+        ClosestEntityChanged.RemoveListener(OnClosestEntityChange);
+    }
 
     private void FixedUpdate()
     {
         updateClosestEntity();
 
-        if (_closestEntity && _transform.TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement))
+        if (_closestEntity)
         {
             Vector3 direction = _closestEntity.transform.position - _transform.position;
             Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-            playerMovement.SetTargetRotation(targetRotation);
 
-            Debug.DrawRay(_transform.position, direction);
+            if (_playerMovement)
+                _playerMovement.SetTargetRotation(targetRotation);
         }
     }
 
     private void updateClosestEntity()
     {
-        Entity closestEntity = null;
+        GameObject closestEntity = null;
         float minDistance = float.PositiveInfinity;
 
-        foreach (Entity entity in _entityList)
+        foreach (GameObject entity in _entityList)
         {
             float distance = Vector3.Distance(_transform.position, entity.transform.position);
             minDistance = Mathf.Min(minDistance, distance);
 
             if (distance == minDistance)
-            {
                 closestEntity = entity;
-            }
         }
 
-        if (_closestEntity != null && _closestEntity != closestEntity)
-            _closestEntity.GetComponent<Renderer>().material.color = Color.white;
+        if (_closestEntity != closestEntity)
+            ClosestEntityChanged.Invoke(closestEntity);
+    }
 
-
-        _closestEntity = closestEntity;
-        if (closestEntity)
-            _closestEntity.GetComponent<Renderer>().material.color = Color.red;
+    private void OnClosestEntityChange(GameObject entity)
+    {
+        _closestEntity = entity;
     }
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.TryGetComponent<Entity>(out Entity entity))
+        if (collider.TryGetComponent<Entity>(out Entity entity))
         {
-            _entityList.Add(entity);
+            _entityList.Add(entity.gameObject);
         }
     }
 
     private void OnTriggerExit(Collider collider)
     {
-        if (collider.gameObject.TryGetComponent<Entity>(out Entity entity))
+        if (collider.TryGetComponent<Entity>(out Entity entity))
         {
-            _entityList.Remove(entity);
+            _entityList.Remove(entity.gameObject);
         }
     }
 }
